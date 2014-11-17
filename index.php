e343372fec0a0d639133b81c4cbc096196895fee
maardz <moldermarge@gmail.com>
2014-11-17 02:07:27 +0200

    Commit

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+   $name ="test";
+   $value =45;
+   $expire = time()  (60*60*24*7);
+   // setcookie($name, $value, $expire);
+   // setcookie("teine", "Tere!", time()  30);
+   //setcookie($name)
+   //setcookie($name, null);
+   //setcookie($name, $value, time() - 3600);
+
+?>
+
 <!DOCTYPE HTML>
 <html>
    <head>
@@ -7,7 +19,24 @@
    </head>
    <body>
 
-   	
+   	<pre>
+      <?php
+         print_r($_COOKIE);
+
+         /*       if (isset($_COOKIE["test"])) {
+            $test = $_COOKIE["test"];
+
+         }else{
+            $test = "";
+         }
+*/
+
+
+
+         $test = isset($_COOKIE["test"]) ? $_COOKIE["test"] : "";
+         echo $test;
+      ?>
+   </pre>
 		
    </body>
 </html>
